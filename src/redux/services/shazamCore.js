ee3664521835b6dbd534638f4b41095562295580
Neyamat Ullah <neyamatullahbulbul@gmail.com>
2022-11-09 11:34:15 +0600

    home page completed

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,21 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const shazamCoreApi = createApi({
+  reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    prepareHeaders: (headers) => {
+      headers.set('X-RapidAPI-Key', '590fa9ba02msh784161029324c04p162b10jsn0c59925041d4');
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => '/charts/world' }),
+  }),
+});
+
+export const { useGetTopChartsQuery } = shazamCoreApi;
+
+
+
